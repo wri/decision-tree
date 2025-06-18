f33d39a72194cd2be4ddda6e2bf8a598949870f4
Jessica Ertel <jessertel@mac.com>
2025-06-18 16:17:21 -0400

    get ids func

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import yaml
 import sys
+import pandas as pd
 sys.path.append('../src/')
 
 import process_api_results as clean
@@ -21,51 +22,57 @@ config_path = "../secrets.yaml"
 with open(config_path) as conf_file:
     config = yaml.safe_load(conf_file)
 
-tm_auth_path = params['tm_api']['tm_auth_path']
-with open(tm_auth_path) as auth_file:
-    auth = yaml.safe_load(auth_file)
 
-headers = {
-    'Authorization': f"Bearer {auth['access_token']}"
-    }
-
+# these need to be defined here becuase they use string formatting
 out = params['outfile']
 today = out['today']
-tm_response = out['tm_response'].format(cohort=out['cohort'],today=out['today'])
-ot_response = out['ot_response'].format(cohort=out['cohort'],today=out['today'])
+tm_outfile = out['tm_response'].format(cohort=out['cohort'],today=out['today'])
+ot_outfile = out['ot_response'].format(cohort=out['cohort'],today=out['today'])
 feats = out['feats'].format(cohort=out['cohort'],today=out['today'])
 feats_maxar_query = out['feats_maxar_query'].format(cohort=out['cohort'],today=out['today'])
 maxar_meta = out['maxar_meta'].format(cohort=out['cohort'],today=out['today'])
 results = out['decision'].format(cohort=out['cohort'],today=out['today'])
+geojson_dir = out['geojsons']
 
+# where should i define the parameters? filenames defined above bc of string
+# formatting and variables defined within the functions?
+# should each step reference the prior variable or the locally saved file?
 
-tm_response = tm.TM_pull_wrapper(params['tm_api']['tm_prod_url'], 
-                             headers, 
-                             c1_ids, # need to identify better way to get ids
-                             tm_response)
+def get_ids(params):
+    out = params['outfile']
+    portfolio = out['portfolio']
+    full = pd.read_csv(portfolio)
+    cohort = out['cohort']
+    keyword = 'terrafund' if cohort == 'c1' else 'terrafund-landscapes'
+    filtered = full[(full.cohort == keyword)]
+    ids = list(set(filtered.project_id))
+    return ids
+ids = get_ids(params)
+
+tm_response = tm.tm_pull_wrapper(params, 
+                             ids, # need to identify better way to get ids
+                             today,
+                             tm_outfile,
+                             geojson_dir)
 
 tm_clean = clean.process_tm_api_results(tm_response, # has to read this file in?
-                                              params['criteria']['drop_missing'],
-                                              outfile1=feats, 
-                                              outfile2=feats_maxar_query)
+                                        params['criteria']['drop_missing'],
+                                        outfile1=feats, 
+                                        outfile2=feats_maxar_query)
 
 ot_response = tm.opentopo_pull_wrapper(params['opt_api']['opt_url'],
-                                       tm_clean,
-                                       params['opt_api']['opt_url'],
                                        config['opentopo_key'],
-                                       outfile=ot_response
-                                        )
+                                       tm_clean,
+                                       geojson_dir,
+                                       params['criteria']['slope_thresh'],
+                                       outfile=ot_outfile)
 
 img_branch = img.analyze_image_availability(tm_clean, # or read in from csv 
-                                                   maxar_meta, # this is a string 
-                                                   tuple(params['criteria']['baseline_range']), 
-                                                   tuple(params['criteria']['ev_range']),
-                                                   params['criteria']['cloud_thresh'])
+                                            maxar_meta, # this is a string 
+                                            params)
 
-canopy_branch = cover.apply_canopy_classification(img_branch,
-                                                params['criteria']['canopy_threshold'],
-                                                tuple(params['criteria']['baseline_range']), 
-                                                tuple(params['criteria']['ev_range']))
+canopy_branch = cover.apply_canopy_classification(img_branch, params)
+
 slope_branch = slope.calculate_slope()
 
 c1_final = tree.apply_rules_baseline(canopy_branch, 

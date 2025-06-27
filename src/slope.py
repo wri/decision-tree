0b41f05fc893fa203333876c839aba428a6fce4f
Jessica Ertel <jessertel@mac.com>
2025-06-27 16:16:35 -0400

    finalize branch

--- src/slope.py
+++ src/slope.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import yaml
 
-def apply_slope_classification(params, projects, slope_stats):
+def apply_slope_classification(params, df, slope_stats):
     '''
     Classify polygons based on the % of the area that is steep (>20% slope).
     
@@ -14,16 +14,20 @@ def apply_slope_classification(params, projects, slope_stats):
     - "unknown" if area_stat is NaN (no data)
     Applies to all polygons, but downstream decision tree will filter for "remote" rows.
     '''
+    n_projects = slope_stats['project_id'].nunique() 
+    print(f"Analyzing slope for {n_projects} projects...")
+
     slope_thresh = params['criteria']['slope_thresh']
     slope_stats = slope_stats[['project_id', 'poly_id', 'slope_area']]
-    slope_stats['slope'] = np.where(slope_stats['slope_area'].isna(), 'unknown',
-                                    np.where(slope_stats['slope_area'] > slope_thresh,'steep','flat'
-                                    )
-                                )
-    comb = projects.merge(
-        slope_stats[['project_id', 'poly_id', 'slope_area', 'slope']], 
+    new_slope = np.where(slope_stats['slope_area'].isna(), 'unknown',
+                np.where(slope_stats['slope_area'] > slope_thresh, 'steep','flat'))
+    slope_stats.loc[:, 'slope'] = new_slope
+    
+    # this will drop any rows that are not in main df
+    comb = df.merge(
+        slope_stats[['project_id', 'poly_id', 'slope_area']], 
         on=['project_id', 'poly_id'], 
-        how='left'
+        how='left' 
     )
     return comb
 

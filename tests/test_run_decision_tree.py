9558a21f058511dd8ef46f1ae913fcda458260f8
Kenn Cartier <kenn.cartier@wri.org>
2026-03-05 09:28:02 -0800

    disabled tests using pytest decoration

--- tests/test_run_decision_tree.py
+++ tests/test_run_decision_tree.py
@@ -1,5 +1,6 @@
 import math
 import os
+import pytest
 
 from decision_tree.run_decision_tree import VerificationDecisionTree
 from decision_tree.tools import get_project_root
@@ -12,81 +13,84 @@ TEST_DIR = os.path.join(ROOT_PATH, "tests")
 TEST_SECRETS_PATH = os.path.join(TEST_DIR, "secrets.yaml")
 
 
-# TODO - Most tests are commented out pending creation of imaginary project data by John
-# def test_run_decision_tree_full():
-#     params_path = os.path.join(TEST_DIR, "params_full.yaml")
-#     workflow = main(params_path, SECRETS_PATH, parse_only=True)
-#
-#     slope_statistics, poly_results, prj_results = workflow.run_decision_tree(None)
-#
-#     # verify that some number of projects were returned
-#     assert len(prj_results) > 1
-#
-#
-# def test_run_decision_tree_id_list():
-#     params_path = os.path.join(TEST_DIR, "params_id_list.yaml")
-#     workflow = main(params_path, SECRETS_PATH, parse_only=True)
-#
-#     project_ids = ['d1f355a2-3e0f-4ffd-bb2f-eec104bf8442', '5fb3f47f-c209-4752-adc6-dea2c0de02de']
-#     slope_statistics, poly_results, prj_results = workflow.run_decision_tree(project_ids)
-#
-#     # Clean up scratch file
-#     sub_folder = 'slope/project_statistics'
-#     delete_scratch_file(sub_folder, 'GHA_22_TILAA_slope_stats.csv')
-#     delete_scratch_file(sub_folder, 'TAZ_22_KIJANIP_slope_stats.csv')
-#
-#     # verify that two projects were returned
-#     expected_project_count = 2
-#     assert len(prj_results) == expected_project_count
-#
-#     sample_project_id = 'd1f355a2-3e0f-4ffd-bb2f-eec104bf8442'
-#     expected_sum_median_slope = 481.5
-#     actual_sum_median_slope = slope_statistics[slope_statistics['project_id'] == sample_project_id]['median_slope'].sum()
-#     assert math.isclose(actual_sum_median_slope, expected_sum_median_slope, rel_tol=0.1), f"Expected {expected_sum_median_slope}, got {actual_sum_median_slope}"
-#
-#     expected_project_label = 'review required'
-#     actual_project_label = prj_results[prj_results['project_id'] == sample_project_id]['baseline_project_label'].values[0]
-#     assert actual_project_label == expected_project_label, f"Expected: {expected_project_label!r}, Actual: {actual_project_label!r}"
-#
-#
-# def test_run_decision_tree_partial():
-#     params_path = os.path.join(TEST_DIR, "params_partial.yaml")
-#     workflow = main(params_path, SECRETS_PATH, parse_only=True)
-#
-#     slope_statistics, poly_results, prj_results = workflow.run_decision_tree(None)
-#
-#     # Clean up scratch file
-#     sub_folder = 'slope/project_statistics'
-#     delete_scratch_file(sub_folder, 'RWA_22_ICRAF_slope_stats.csv')
-#     delete_scratch_file(sub_folder, 'TGO_22_APAF_slope_stats.csv')
-#
-#     # verify that two projects were returned
-#     expected_project_count = 2
-#     assert len(prj_results) == expected_project_count
-#
-#     sample_project_id = 'f81c1422-025c-45b1-a2e1-d354177523ca'
-#     expected_sum_median_slope = 307.4
-#     actual_sum_median_slope = slope_statistics[slope_statistics['project_id'] == sample_project_id]['median_slope'].sum()
-#     assert math.isclose(actual_sum_median_slope, expected_sum_median_slope, rel_tol=0.1), f"Expected {expected_sum_median_slope}, got {actual_sum_median_slope}"
-#
-#     expected_project_label = 'strong remote'
-#     actual_project_label = prj_results[prj_results['project_id'] == sample_project_id]['baseline_project_label'].values[0]
-#     assert actual_project_label == expected_project_label, f"Expected: {expected_project_label!r}, Actual: {actual_project_label!r}"
-#
-# def test_run_decision_tree_score():
-#     params_path = os.path.join(TEST_DIR, "params_score.yaml")
-#     workflow = main(params_path, SECRETS_PATH, parse_only=True)
-#
-#     slope_statistics, poly_results, prj_results = workflow.run_decision_tree(None)
-#
-#     # verify that two projects were returned
-#     expected_project_count = 2
-#     assert len(prj_results) == expected_project_count
-# 
-#     sample_project_id = 'f81c1422-025c-45b1-a2e1-d354177523ca'
-#     expected_project_label = 'strong remote'
-#     actual_project_label = prj_results[prj_results['project_id'] == sample_project_id]['baseline_project_label'].values[0]
-#     assert actual_project_label == expected_project_label, f"Expected: {expected_project_label!r}, Actual: {actual_project_label!r}"
+@pytest.mark.skip(reason="This test is under development and waiting for release of test projects")
+def test_run_decision_tree_full():
+    params_path = os.path.join(TEST_DIR, "params_full.yaml")
+    workflow = main(params_path, SECRETS_PATH, parse_only=True)
+
+    slope_statistics, poly_results, prj_results = workflow.run_decision_tree(None)
+
+    # verify that some number of projects were returned
+    assert len(prj_results) > 1
+
+
+@pytest.mark.skip(reason="This test is under development and waiting for release of test projects")
+def test_run_decision_tree_id_list():
+    params_path = os.path.join(TEST_DIR, "params_id_list.yaml")
+    workflow = main(params_path, SECRETS_PATH, parse_only=True)
+
+    project_ids = ['d1f355a2-3e0f-4ffd-bb2f-eec104bf8442', '5fb3f47f-c209-4752-adc6-dea2c0de02de']
+    slope_statistics, poly_results, prj_results = workflow.run_decision_tree(project_ids)
+
+    # Clean up scratch file
+    sub_folder = 'slope/project_statistics'
+    delete_scratch_file(sub_folder, 'GHA_22_TILAA_slope_stats.csv')
+    delete_scratch_file(sub_folder, 'TAZ_22_KIJANIP_slope_stats.csv')
+
+    # verify that two projects were returned
+    expected_project_count = 2
+    assert len(prj_results) == expected_project_count
+
+    sample_project_id = 'd1f355a2-3e0f-4ffd-bb2f-eec104bf8442'
+    expected_sum_median_slope = 481.5
+    actual_sum_median_slope = slope_statistics[slope_statistics['project_id'] == sample_project_id]['median_slope'].sum()
+    assert math.isclose(actual_sum_median_slope, expected_sum_median_slope, rel_tol=0.1), f"Expected {expected_sum_median_slope}, got {actual_sum_median_slope}"
+
+    expected_project_label = 'review required'
+    actual_project_label = prj_results[prj_results['project_id'] == sample_project_id]['baseline_project_label'].values[0]
+    assert actual_project_label == expected_project_label, f"Expected: {expected_project_label!r}, Actual: {actual_project_label!r}"
+
+
+@pytest.mark.skip(reason="This test is under development and waiting for release of test projects")
+def test_run_decision_tree_partial():
+    params_path = os.path.join(TEST_DIR, "params_partial.yaml")
+    workflow = main(params_path, SECRETS_PATH, parse_only=True)
+
+    slope_statistics, poly_results, prj_results = workflow.run_decision_tree(None)
+
+    # Clean up scratch file
+    sub_folder = 'slope/project_statistics'
+    delete_scratch_file(sub_folder, 'RWA_22_ICRAF_slope_stats.csv')
+    delete_scratch_file(sub_folder, 'TGO_22_APAF_slope_stats.csv')
+
+    # verify that two projects were returned
+    expected_project_count = 2
+    assert len(prj_results) == expected_project_count
+
+    sample_project_id = 'f81c1422-025c-45b1-a2e1-d354177523ca'
+    expected_sum_median_slope = 307.4
+    actual_sum_median_slope = slope_statistics[slope_statistics['project_id'] == sample_project_id]['median_slope'].sum()
+    assert math.isclose(actual_sum_median_slope, expected_sum_median_slope, rel_tol=0.1), f"Expected {expected_sum_median_slope}, got {actual_sum_median_slope}"
+
+    expected_project_label = 'strong remote'
+    actual_project_label = prj_results[prj_results['project_id'] == sample_project_id]['baseline_project_label'].values[0]
+    assert actual_project_label == expected_project_label, f"Expected: {expected_project_label!r}, Actual: {actual_project_label!r}"
+
+@pytest.mark.skip(reason="This test is under development and waiting for release of test projects")
+def test_run_decision_tree_score():
+    params_path = os.path.join(TEST_DIR, "params_score.yaml")
+    workflow = main(params_path, SECRETS_PATH, parse_only=True)
+
+    slope_statistics, poly_results, prj_results = workflow.run_decision_tree(None)
+
+    # verify that two projects were returned
+    expected_project_count = 2
+    assert len(prj_results) == expected_project_count
+
+    sample_project_id = 'f81c1422-025c-45b1-a2e1-d354177523ca'
+    expected_project_label = 'strong remote'
+    actual_project_label = prj_results[prj_results['project_id'] == sample_project_id]['baseline_project_label'].values[0]
+    assert actual_project_label == expected_project_label, f"Expected: {expected_project_label!r}, Actual: {actual_project_label!r}"
 
 
 def test_run_decision_tree_param_parsing():

3449bdbe516babfe0d8de2454b2966719ccad2dc
Jessica <jessertel@mac.com>
2026-04-10 16:40:10 -0400

    update to consider img count and timeframe

--- decision_tree/image_availability.py
+++ decision_tree/image_availability.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-def analyze_image_availability(params, proj_df, maxar_fp):
+def analyze_image_availability(params, 
+                               proj_df: pd.Dataframe, 
+                               maxar_fp: str):
     """
     Assesses image availability for baseline & early verification per 
     project/polygon based on user defined windows.
@@ -19,10 +21,8 @@ def analyze_image_availability(params, proj_df, maxar_fp):
 
     criteria = params.get('criteria', {})
     baseline_range = tuple(criteria.get('baseline_range'))
+    ext_baseline_range = tuple(criteria.get('ext_baseline_range'))
     ev_range = tuple(criteria.get('ev_range'))
-    cloud_thresh = criteria.get('cloud_thresh')
-    off_nadir = criteria.get('off_nadir')
-    sun = criteria.get('sun_elevation')
     
     proj_df.columns = proj_df.columns.str.lower()
     img_df = pd.read_csv(maxar_fp, dtype={"datetime": "string"})
@@ -31,7 +31,8 @@ def analyze_image_availability(params, proj_df, maxar_fp):
             'datetime',
             'area:cloud_cover_percentage',
             'view:sun_elevation',
-            'area:avg_off_nadir_angle'
+            'area:avg_off_nadir_angle',
+            'area:coverage_fraction'
             ]].copy()
 
     img_df["img_date"] = pd.to_datetime(
@@ -40,46 +41,48 @@ def analyze_image_availability(params, proj_df, maxar_fp):
         errors="coerce"
         )
 
-    # Ensure correct datatypes
-    num_cols = ['area:cloud_cover_percentage', 'view:sun_elevation', 'area:avg_off_nadir_angle']
-    img_df[num_cols] = img_df[num_cols].apply(pd.to_numeric, errors='coerce')
+    # Ensure correct datatypes & merge
     proj_df['plantstart'] = pd.to_datetime(proj_df['plantstart'], errors='coerce')
-
-    # merge img df and feats
     merged = img_df.merge(proj_df, on=['project_id', 'poly_id'], how='left')
+    # add step here to check if any rows were dropped
 
     # Compute image availability window
     merged['date_diff'] = (merged['img_date'] - merged['plantstart']).dt.days
 
     baseline = merged[
         (merged['date_diff'] >= baseline_range[0]) &
-        (merged['date_diff'] <= baseline_range[1]) &
-        (merged['area:cloud_cover_percentage'] < cloud_thresh) &
-        (merged['view:sun_elevation'] >= sun) &
-        (merged['area:avg_off_nadir_angle'] <= off_nadir)
+        (merged['date_diff'] <= baseline_range[1])
     ]
     baseline_summary = (
         baseline.groupby(['project_id', 'poly_id'])
         .size()
         .reset_index(name='baseline_img_count')
     )
+    baseline_ext = merged[
+        (merged['date_diff'] >= ext_baseline_range[0]) &
+        (merged['date_diff'] <= ext_baseline_range[1])
+    ]
+    baseline_ext_summary = (
+        baseline_ext.groupby(['project_id', 'poly_id'])
+        .size()
+        .reset_index(name='baseline_ext_img_count')
+    )
+
     ev = merged[
         (merged['date_diff'] >= ev_range[0]) &
-        (merged['date_diff'] <= ev_range[1]) &
-        (merged['area:cloud_cover_percentage'] < cloud_thresh) &
-        (merged['view:sun_elevation'] >= sun) &
-        (merged['area:avg_off_nadir_angle'] <= off_nadir)
+        (merged['date_diff'] <= ev_range[1]) 
     ]
     ev_summary = (
         ev.groupby(['project_id', 'poly_id'])
         .size()
         .reset_index(name='ev_img_count')
     )
-    final_summary = proj_df.merge(baseline_summary, on=['project_id', 'poly_id'], how='left') \
-                           .merge(ev_summary, on=['project_id', 'poly_id'], how='left')
+    final_summary = proj_df \
+    .merge(baseline_summary,     on=['project_id', 'poly_id'], how='left') \
+    .merge(baseline_ext_summary, on=['project_id', 'poly_id'], how='left') \
+    .merge(ev_summary,           on=['project_id', 'poly_id'], how='left')
 
-    # Fill in missing image counts with 0
-    final_summary[['baseline_img_count', 'ev_img_count']] = final_summary[
-        ['baseline_img_count', 'ev_img_count']
-    ].fillna(0)
+    final_summary[['baseline_img_count', 'baseline_ext_img_count', 'ev_img_count']] = \
+        final_summary[['baseline_img_count', 'baseline_ext_img_count', 'ev_img_count']].fillna(0)
+    
     return final_summary
